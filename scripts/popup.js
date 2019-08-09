44eb5e75079766d887d7e99d93ee2931667a7861
Roman Yakovlev <tensegrity.666@icloud.com>
2019-08-10 00:54:08 +0300

    Удаляет лишние пробелы в вёрстке, добавляет минифицированные файлы

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,15 +1,13 @@
 var link = document.querySelector(".js-search-button");
-  var popup = document.querySelector(".modal");
+var popup = document.querySelector(".modal");
   
   link.addEventListener("click", function (evt) {
     if (popup.classList.contains("modal-show")) {
         evt.preventDefault();
-        console.log("скрывает окно");
         popup.classList.remove("modal-show");
         popup.classList.add("modal-close");
         } else {
           evt.preventDefault();
-          console.log("показывает окно");
           popup.classList.remove("modal-close");
           popup.classList.add("modal-show");
         }
